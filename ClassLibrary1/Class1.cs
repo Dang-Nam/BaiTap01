679564ab6332cda80a9ef692fce063423ec2ccaa
NAM ĐẶNG <nambbccddee@gmail.com>
2024-09-09 09:43:20 +0700

    all cs2

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class SalaryCalculator
+    {
+        // Phương thức tính lương dựa trên số ngày làm việc và tiền công mỗi ngày
+        public double CalculateSalary(int workDays, double dailyWage)
+        {
+            // Tính lương: số ngày làm việc * tiền công mỗi ngày
+            return workDays * dailyWage;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,30 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ClassLibrary1;  // Thêm namespace của thư viện DLL
 
-namespace BaiToanTinhLuong
+namespace ConsoleApp2
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            // Nhập số ngày công làm việc
+            // Nhập số ngày công và tiền công mỗi ngày
             Console.Write("Nhập số ngày công: ");
             int workDays = Convert.ToInt32(Console.ReadLine());
 
-            // Nhập tiền công mỗi ngày
             Console.Write("Nhập tiền công mỗi ngày: ");
             double dailyWage = Convert.ToDouble(Console.ReadLine());
 
-            // Tính tổng lương
-            double totalSalary = workDays * dailyWage;
+            // Sử dụng lớp SalaryCalculator từ DLL
+            SalaryCalculator calculator = new SalaryCalculator();
 
-            // Xuất kết quả
+            // Tính lương
+            double totalSalary = calculator.CalculateSalary(workDays, dailyWage);
+
+            // Hiển thị kết quả
             Console.WriteLine("Tổng lương: " + totalSalary);
 
             // Giữ cửa sổ console mở
             Console.ReadLine();
         }
     }
-
 }
