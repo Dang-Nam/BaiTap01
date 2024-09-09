8a6f8586b32f1b109f575b80aec68ceabee379b6
Dang-Nam <168844237+Dang-Nam@users.noreply.github.com>
2024-09-09 23:35:30 +0700

    Add files via upload

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tính Lương</title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Tính lương</h2>
+            <label for="soNgayLam">Số ngày làm:</label>
+            <asp:TextBox ID="txtSoNgayLam" runat="server"></asp:TextBox>
+            <br /><br />
+            <label for="tienCong">Tiền công mỗi ngày:</label>
+            <asp:TextBox ID="txtTienCong" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="btnTinhLuong" runat="server" Text="Tính lương" OnClick="btnTinhLuong_Click" />
+            <br /><br />
+            <label for="ketQua">Tổng lương:</label>
+            <asp:Label ID="lblTongLuong" runat="server" Text="0"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTinhLuong_Click(object sender, EventArgs e)
+        {
+            int send = 0;
+            try
+            {
+                double ngay_lam = double.Parse(txtSoNgayLam.Text);
+                send = 1;
+                double tinhcong = double.Parse(txtTienCong.Text);
+                SalaryCalculator calculator = new SalaryCalculator();
+                double tongTien = calculator.CalculateSalary(ngay_lam, tinhcong);
+
+                string ketqua = $"{tongTien} VND";
+                lblTongLuong.Text = $"{ketqua}";
+            }
+            catch (Exception ex)
+            {
+                switch (send)
+                {
+                    case 0:
+                        lblTongLuong.Text = "Có lỗi khi nhập số giờ";
+                        break;
+                    case 1:
+                        lblTongLuong.Text = "Có lỗi khi chọn loại máy";
+                        break;
+                    case 2:
+                        lblTongLuong.Text = "Có lỗi khi nhập thông tin điều hòa";
+                        break;
+                    default:
+                        lblTongLuong.Text = $"Có lỗi không xác định: {ex.Message}";
+                        break;
+                }
+
+            }
+            }
+    }
+}
